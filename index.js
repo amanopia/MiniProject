e52c91b49972f1c0731e3d21afbfd45956875067
amanopia <60280815+amanopia@users.noreply.github.com>
2021-03-08 13:21:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Hello World!")
+// Auto animate for demo
+
+var toggle = document.querySelector('.toggle input')
+var animate = setInterval(() => {
+    toggle.checked = !toggle.checked
+}, 3000)
+
+document.querySelector('body').addEventListener('click', () => {
+  clearInterval(animate);
+})
